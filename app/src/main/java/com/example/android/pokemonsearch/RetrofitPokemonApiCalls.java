74c058658a0ca548d4112753f1e4b122d2d23aed
Stephanie Youstra <steph.you07@gmail.com>
2018-04-26 14:26:45 -0400

    Design activity_main layout

---
+++ app/src/main/java/com/example/android/pokemonsearch/RetrofitPokemonApiCalls.java
@@ -0,0 +1,8 @@
+package com.example.android.pokemonsearch;
+
+public interface RetrofitPokemonApiCalls {
+
+    //https://pokeapi.co/docsv2/#pokemon-section
+    //http://code.hootsuite.com/load-images-in-android-using-glide/
+
+}
